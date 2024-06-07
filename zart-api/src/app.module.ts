d7fa93510116112b50ce256aea7713a1a8e714af
Pr3ciousXX <preciousbobmanuel501@gmail.com>
2024-06-07 15:53:27 +0100

    create prisma db, auth module with signin and signup mock enpoints

--- zart-api/src/app.module.ts
+++ zart-api/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [AppController, AuthController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
