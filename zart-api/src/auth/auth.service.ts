f16113f2c78558fa74e87c01bc09823d451b5ccb
Pr3ciousXX <preciousbobmanuel501@gmail.com>
2024-07-17 15:08:04 +0100

    refactor (entire codebase): switch from Prisma to TypeORM

--- zart-api/src/auth/auth.service.ts
+++ zart-api/src/auth/auth.service.ts
@@ -5,57 +5,50 @@ import {
 } from '@nestjs/common';
 import { SigninDto, SignupDto } from './dto';
 import * as argon from 'argon2';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/users.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private prisma: PrismaService,
     private jwt: JwtService,
     private config: ConfigService,
+    @InjectRepository(UserEntity)
+    private readonly userRepo: Repository<UserEntity>,
   ) {}
-  async signUp(dto: SignupDto) {
+
+  async signup(dto: SignupDto) {
     try {
-      const { email, firstName, lastName, password } = dto;
-      const passwordHash = await argon.hash(password);
-      const user = await this.prisma.user.create({
-        data: {
-          firstName,
-          lastName,
-          email,
-          passwordHash,
-        },
-      });
+      const user = this.userRepo.create(dto);
+      await user.save();
       return this.signToken(user.id, user.email);
     } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === 'P2002') {
-          throw new ConflictException('Credentials already exists');
-        }
+      if (e.code === '23505') {
+        throw new ConflictException('Credentials already exists');
       }
       throw e;
     }
   }
 
-  async signIn(dto: SigninDto) {
-    // Find the user by email
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email: dto.email,
-      },
-    });
-    // If user doesn't exist, throw exception
-    if (!user) throw new NotFoundException('Credentials incorrect');
-    // Compare password
-    const validPassword = await argon.verify(user.passwordHash, dto.password);
+  // async signIn(dto: SigninDto) {
+  //   // Find the user by email
+  //   const user = await this.prisma.user.findUnique({
+  //     where: {
+  //       email: dto.email,
+  //     },
+  //   });
+  //   // If user doesn't exist, throw exception
+  //   if (!user) throw new NotFoundException('Credentials incorrect');
+  //   // Compare password
+  //   const validPassword = await argon.verify(user.passwordHash, dto.password);
 
-    // If password incorrect throw exception
-    if (!validPassword) throw new NotFoundException('Credentials incorrect');
-    return this.signToken(user.id, user.email);
-  }
+  //   // If password incorrect throw exception
+  //   if (!validPassword) throw new NotFoundException('Credentials incorrect');
+  //   return this.signToken(user.id, user.email);
+  // }
 
   async signToken(
     userId: string,
