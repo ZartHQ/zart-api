c94326dfe00e1167c5bf948ca155f95987a803b2
Pr3ciousXX <preciousbobmanuel501@gmail.com>
2024-08-11 15:01:45 +0100

    feat(locations): build locations controller#

--- zart-api/src/locations/locations.controller.ts
+++ zart-api/src/locations/locations.controller.ts
@@ -1,10 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { LocationsService } from './locations.service';
+import { LocationDto } from './dto/locations.dto';
+import { LocationEntity } from './locations.entity';
 
 @Controller('locations')
 export class LocationsController {
-  constructor(private readonly locationService: LocationsService){}
+  constructor(private readonly locationService: LocationsService) {}
 
-  // @post()
+  @Post()
+  create(@Body() dto: LocationDto): Promise<LocationEntity> {
+    const location = this.locationService.create(dto);
+    return location;
+  }
 
+  @Get()
+  findAll(): Promise<LocationEntity[]> {
+    const locations = this.locationService.findAll();
+    return locations;
+  }
+
+  @Get('area')
+  findByArea(@Param('area') areaParam: string): Promise<LocationEntity> {
+    const area = this.locationService.findByArea(areaParam);
+    return area;
+  }
 }
