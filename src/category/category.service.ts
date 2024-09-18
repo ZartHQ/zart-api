adb6cf29eab8923c2f40085974c08b463271f068
temitope-paystack <temitope.agbaje@paystack.com>
2024-09-18 12:46:30 +0100

    add Category endpoints

---
+++ src/category/category.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateCategoryDto, UpdateCategoryDto } from "src/dto/category.dto";
+import { Category } from "src/entities/category.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>
+  ) {}
+
+  addCategory(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const category = this.categoryRepository.create(createCategoryDto);
+    return this.categoryRepository.save(category);
+  }
+
+  findAllCategories(): Promise<Category[]> {
+    return this.categoryRepository.find();
+  }
+
+  findOneCategory(id: number): Promise<Category> {
+    return this.categoryRepository.findOneBy({ id });
+  }
+
+  async updateCategory(id: number, updateCategoryDto: UpdateCategoryDto): Promise<Category> {
+    const category = await this.categoryRepository.preload({
+      id: id,
+      ...updateCategoryDto,
+    });
+    
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+    
+    return this.categoryRepository.save(category);
+  }
+
+  async deleteCategory(id: number): Promise<void> {
+    await this.categoryRepository.delete(id);
+  }
+}
